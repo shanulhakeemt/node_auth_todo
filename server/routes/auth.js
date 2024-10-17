23af2b37c551dc2dd7439983e513a54d25063b1d
shanul hakeem <shanulhakeem60@gmail.com>
2024-10-17 14:15:39 +0530

    signup route completed

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,20 +1,26 @@
 const express = require("express");
+const bcryptjs = require("bcryptjs");
+const User = require("../models/user");
 const authRouter = express.Router();
 
-authRouter.post("auth/signup", async(req, res) => {
+authRouter.post("/auth/signup", async(req, res) => {
 
     try {
 
         const { name, email, password } = req.body;
-        const existingUser = User.findOne({ email });
+        const existingUser = await User.findOne({ email });
         if (existingUser) {
             return res.status(400).json({ msg: "User with same email aleady exist" });
         }
-        const hashedPassword = bcryptjs.hash(password, 8);
+        const hashedPassword = await bcryptjs.hash(password, 8);
 
         let user = new User({ email: email, password: hashedPassword, name: name });
+        user = await user.save();
+        res.json(user);
 
-    } catch (e) {}
+    } catch (e) {
+        res.status(500).json({ error: e.message });
+    }
 
 });
 
